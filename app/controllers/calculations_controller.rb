57e810d418a8b3b45732261bf25e12fd5baf2b96
Brooke K <brookekingsland@users.noreply.github.com>
2017-04-21 11:29:22 -0500

    Started on the square forms.

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -13,4 +13,14 @@ class CalculationsController < ApplicationController
     render("calculations/flex_square_root.html.erb")
   end
 
+  def square_form
+    render("calculations/square_form.html.erb")
+  end
+
+  def square
+    @user_provided_number=params["user_input_square"].to_f
+    @squared_number=@user_provided_number**2
+    render("calculations/square.html.erb")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,17 @@ Rails.application.routes.draw do
 get("/flexible/square/:the_number",
 {:controller=>"calculations",
 :action=>"flex_square"})
+
 get("/flexible/square_root/:the_number",
 {:controller=>"calculations",
 :action=>"flex_square_root"})
+
+get("/square/new",
+{:controller=>"calculations",
+:action=>"square_form"})
+
+get("/square/results",
+{:controller=>"calculations",
+:action=>"square"})
+
 end
